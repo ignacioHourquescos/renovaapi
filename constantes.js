734ba5dd12dc4d15116a3ec3862953c8509d7a01
jimancio <ihourquescos@gmail.com>
2020-10-06 12:27:13 -0300

    propiedad descuentos

--- constantes.js
+++ constantes.js
@@ -1,13 +1,13 @@
 const listas=[
-    {"codigo":1,    "descripcion":"FRAM",               "aumento":"Octubre: Sin aumento",                         "descuento":"Aplicar descuento corriente"},
+    {"codigo":1,    "descripcion":"FRAM",               "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento corriente"},
     {"codigo":12,   "descripcion":"MOTUL",              "aumento":"Octubre: +30% solo refrigerantes",    "descuento":"Aplicar descuento corriente"},       
-    {"codigo":3,    "descripcion":"VALVOLINE",          "aumento":"Octubre: Sin aumento",                         "descuento":"Aplicar descuento corriente"},      
-    {"codigo":316,  "descripcion":"PETRONAS",           "aumento":"Octubre: Sin aumento",                         "descuento":"Aplicar descuento corriente"},         
-    {"codigo":5,    "descripcion":"TOTAL",              "aumento":"Octubre: Sin aumento",                         "descuento":"Aplicar descuento corriente"},     
+    {"codigo":3,    "descripcion":"VALVOLINE",          "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento corriente"},      
+    {"codigo":316,  "descripcion":"PETRONAS",           "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento corriente"},         
+    {"codigo":5,    "descripcion":"TOTAL",              "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento corriente"},     
     {"codigo":14,   "descripcion":"LOCX",               "aumento":"Octubre: +7%",                        "descuento":"Aplicar descuento corriente"}, 
-    {"codigo":11,   "descripcion":"WAGNER LOCKHEED",    "aumento":"Octubre: Sin aumento",                         "descuento": "10% por 12 unidades surtidas"},
-    {"codigo":13,   "descripcion":"TRICO",              "aumento":"Octubre: +10%",                       "descuento":"10% por caja cerrada surtida"},
-    {"codigo":8,    "descripcion":"RAMA",               "aumento":"OCtubre: +10%",                       "descuento": "10% por 12 unidades surtidas"}
+    {"codigo":11,   "descripcion":"WAGNER LOCKHEED",    "aumento":"Octubre: Sin aumento",                "descuento":"10% por 12 unidades surtidas"},
+    {"codigo":13,   "descripcion":"TRICO",              "aumento":"Octubre: +10%",                       "descuento":"10% por 12 undiades surtidas"},
+    {"codigo":8,    "descripcion":"RAMA",               "aumento":"OCtubre: +10%",                       "descuento":"10% por 12 unidades surtidas"}
 ];
 
 module.exports={
