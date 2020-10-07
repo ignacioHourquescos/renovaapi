ee581f61f744211c2bfa67a1f32d0605210626c0
jimancio <ihourquescos@gmail.com>
2020-10-06 22:00:37 -0300

    cambio texto de descuentos

--- constantes.js
+++ constantes.js
@@ -4,10 +4,10 @@ const listas=[
     {"codigo":3,    "descripcion":"VALVOLINE",          "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento correspondiente"},      
     {"codigo":316,  "descripcion":"PETRONAS",           "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento correspondiente"},         
     {"codigo":5,    "descripcion":"TOTAL",              "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento correspondiente"},     
-    {"codigo":14,   "descripcion":"LOCX",               "aumento":"Octubre: +7%",                        "descuento":"10% por 12 unidades surtidas"}, 
-    {"codigo":11,   "descripcion":"WAGNER LOCKHEED",    "aumento":"Octubre: Sin aumento",                "descuento":"10% por 12 unidades surtidas"},
-    {"codigo":13,   "descripcion":"TRICO",              "aumento":"Octubre: +10%",                       "descuento":"10% por 12 undiades surtidas"},
-    {"codigo":8,    "descripcion":"RAMA",               "aumento":"OCtubre: +10%",                       "descuento":"Sin descuento"}
+    {"codigo":14,   "descripcion":"LOCX",               "aumento":"Octubre: +7%",                        "descuento":""}, 
+    {"codigo":11,   "descripcion":"WAGNER LOCKHEED",    "aumento":"Octubre: Sin aumento",                "descuento":""},
+    {"codigo":13,   "descripcion":"TRICO",              "aumento":"Octubre: +10%",                       "descuento":""},
+    {"codigo":8,    "descripcion":"RAMA",               "aumento":"OCtubre: +10%",                       "descuento":""}
 ];
 
 module.exports={
