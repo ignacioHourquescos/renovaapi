91f70b8c70356b2bbfab41c38b0aefcb561a255d
jimancio <ihourquescos@gmail.com>
2020-10-08 08:56:59 -0300

    cambie descuentos

--- constantes.js
+++ constantes.js
@@ -2,11 +2,11 @@ const listas=[
     {"codigo":1,    "descripcion":"FRAM",               "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento correspondiente"},
     {"codigo":12,   "descripcion":"MOTUL",              "aumento":"Octubre: +30% solo refrigerantes",    "descuento":"Aplicar descuento correspondiente"},       
     {"codigo":3,    "descripcion":"VALVOLINE",          "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento correspondiente"},      
-    {"codigo":316,  "descripcion":"PETRONAS",           "aumento":"Octubre: +7%",                "descuento":"Aplicar descuento correspondiente"},         
+    {"codigo":316,  "descripcion":"PETRONAS",           "aumento":"Octubre: +7%",                        "descuento":"Aplicar descuento correspondiente"},         
     {"codigo":5,    "descripcion":"TOTAL",              "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento correspondiente"},     
-    {"codigo":14,   "descripcion":"LOCX",               "aumento":"Octubre: +7%",                        "descuento":""}, 
-    {"codigo":11,   "descripcion":"WAGNER LOCKHEED",    "aumento":"Octubre: Sin aumento",                "descuento":""},
-    {"codigo":13,   "descripcion":"TRICO",              "aumento":"Octubre: +10%",                       "descuento":""},
+    {"codigo":14,   "descripcion":"LOCX",               "aumento":"Octubre: +7%",                        "descuento":"Descuento del 10% comprando 10 unidades surtidas"}, 
+    {"codigo":11,   "descripcion":"WAGNER LOCKHEED",    "aumento":"Octubre: Aumento Desparejo",          "descuento":"Descuento del 10% comprando 10 unidades surtidas"},
+    {"codigo":13,   "descripcion":"TRICO",              "aumento":"Octubre: Auemnto Desparejo ",         "descuento":"Descuento del 10% comprando 10 unidades surtidas"},
     {"codigo":8,    "descripcion":"RAMA",               "aumento":"OCtubre: +10%",                       "descuento":""}
 ];
 
