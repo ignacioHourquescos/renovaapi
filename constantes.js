b83077761d8206f94bd31e55db10fc5ab839890a
jimancio <ihourquescos@gmail.com>
2020-10-08 08:52:57 -0300

    cambie auemtno de petronas

--- constantes.js
+++ constantes.js
@@ -2,7 +2,7 @@ const listas=[
     {"codigo":1,    "descripcion":"FRAM",               "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento correspondiente"},
     {"codigo":12,   "descripcion":"MOTUL",              "aumento":"Octubre: +30% solo refrigerantes",    "descuento":"Aplicar descuento correspondiente"},       
     {"codigo":3,    "descripcion":"VALVOLINE",          "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento correspondiente"},      
-    {"codigo":316,  "descripcion":"PETRONAS",           "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento correspondiente"},         
+    {"codigo":316,  "descripcion":"PETRONAS",           "aumento":"Octubre: +7%",                "descuento":"Aplicar descuento correspondiente"},         
     {"codigo":5,    "descripcion":"TOTAL",              "aumento":"Octubre: Sin aumento",                "descuento":"Aplicar descuento correspondiente"},     
     {"codigo":14,   "descripcion":"LOCX",               "aumento":"Octubre: +7%",                        "descuento":""}, 
     {"codigo":11,   "descripcion":"WAGNER LOCKHEED",    "aumento":"Octubre: Sin aumento",                "descuento":""},
