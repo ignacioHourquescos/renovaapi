eb4de2adc10e74115a51b80f51914c286826539d
jimancio <ihourquescos@gmail.com>
2020-11-02 09:01:51 -0300

    aumetnos noviembre

--- constantes.js
+++ constantes.js
@@ -1,11 +1,11 @@
 const listas=[
-    {"codigo":1,    "descripcion":"FRAM",              "mes":"Octubre", "aumento":"Sin aumento",                "descuento":"Consultar descuento x cantidad"},
-    {"codigo":12,   "descripcion":"MOTUL",             "mes":"Octubre", "aumento":"+ 30% solo refrigerantes",    "descuento":"10% Descuento x caja cerrada"},       
+    {"codigo":1,    "descripcion":"FRAM",              "mes":"Noviembre", "aumento":"+ 6%",                "descuento":"Consultar descuento x cantidad"},
+    {"codigo":12,   "descripcion":"MOTUL",             "mes":"Noviembre", "aumento":"+ 5%",                "descuento":"10% Descuento x caja cerrada"},       
     {"codigo":3,    "descripcion":"VALVOLINE",         "mes":"Octubre", "aumento":"+ 5% aprox (baja en transmision y refrigerantes)",            "descuento":"10% Descuento x caja cerrada"},      
     {"codigo":316,  "descripcion":"PETRONAS",          "mes":"Octubre", "aumento":"+ 7%",                        "descuento":"10% Descuento x caja cerrada"},         
     {"codigo":5,    "descripcion":"TOTAL",             "mes":"Octubre", "aumento":"Sin aumento",                "descuento":"10% Descuento x caja cerrada"},     
     {"codigo":14,   "descripcion":"LOCX",              "mes":"Octubre", "aumento":"+ 7%",                        "descuento":"10% Descuento x 10 unidades surtidas"}, 
-    {"codigo":11,   "descripcion":"WAGNER LOCKHEED",   "mes":"Octubre", "aumento":"Aumento Desparejo",          "descuento":"10% Descuento x 10 unidades surtidas"},
+    {"codigo":11,   "descripcion":"WAGNER LOCKHEED",   "mes":"Noviembre", "aumento":"+ 5%",          "descuento":"10% Descuento x 10 unidades surtidas"},
     {"codigo":13,   "descripcion":"TRICO",             "mes":"Octubre", "aumento":"Aumento Desparejo ",         "descuento":"5% Descuento x 10 unidades surtidas"},
     {"codigo":8,    "descripcion":"RAMA",              "mes":"Octubre", "aumento":"+ 10%",                       "descuento":"Consultar descuento x cantidad"}
 ];
