6ae4fb18d1b4973c61b974bf54aa943bdc658c31
jimancio <ihourquescos@gmail.com>
2020-11-02 15:27:21 -0300

    autmeo locx

--- constantes.js
+++ constantes.js
@@ -4,7 +4,7 @@ const listas=[
     {"codigo":3,    "descripcion":"VALVOLINE",         "mes":"Octubre", "aumento":"+ 5% aprox (baja en transmision y refrigerantes)",            "descuento":"10% Descuento x caja cerrada"},      
     {"codigo":316,  "descripcion":"PETRONAS",          "mes":"Octubre", "aumento":"+ 7%",                        "descuento":"10% Descuento x caja cerrada"},         
     {"codigo":5,    "descripcion":"TOTAL",             "mes":"Noviembre", "aumento":"+ 10%",                "descuento":"10% Descuento x caja cerrada"},     
-    {"codigo":14,   "descripcion":"LOCX",              "mes":"Octubre", "aumento":"+ 7%",                        "descuento":"10% Descuento x 10 unidades surtidas"}, 
+    {"codigo":14,   "descripcion":"LOCX",              "mes":"Noviembre", "aumento":"+ 8%",                        "descuento":"10% Descuento x 10 unidades surtidas"}, 
     {"codigo":11,   "descripcion":"WAGNER LOCKHEED",   "mes":"Noviembre", "aumento":"+ 5%",          "descuento":"10% Descuento x 10 unidades surtidas"},
     {"codigo":13,   "descripcion":"TRICO",             "mes":"Octubre", "aumento":"Aumento Desparejo ",         "descuento":"5% Descuento x 10 unidades surtidas"},
     {"codigo":8,    "descripcion":"RAMA",              "mes":"Octubre", "aumento":"+ 10%",                       "descuento":"Consultar descuento x cantidad"}
