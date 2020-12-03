57bd2413cbd470eb37a8caaf539d213ab0b961c8
jimancio <ihourquescos@gmail.com>
2020-12-03 09:57:39 -0300

    auemntos dic

--- constantes.js
+++ constantes.js
@@ -2,12 +2,12 @@ const listas=[
     {"codigo":1,    "descripcion":"FRAM",              "mes":"Noviembre", "aumento":"+ 6%",                "descuento":"Consultar descuento x cantidad"},
     {"codigo":12,   "descripcion":"MOTUL",             "mes":"Diciembre", "aumento":"+ 4%",                "descuento":"10% Descuento x caja cerrada"},       
     {"codigo":3,    "descripcion":"VALVOLINE",         "mes":"Octubre", "aumento":"+ 5% aprox ",            "descuento":"10% Descuento x caja cerrada"},      
-    {"codigo":316,  "descripcion":"PETRONAS",          "mes":"Noviembre", "aumento":"+ 5% aprox",                        "descuento":"10% Descuento x caja cerrada"},         
+    {"codigo":316,  "descripcion":"PETRONAS",          "mes":"Diciembre", "aumento":"+ 5% (+15% refrigerantes",                        "descuento":"10% Descuento x caja cerrada"},         
     {"codigo":5,    "descripcion":"TOTAL",             "mes":"Noviembre", "aumento":"+ 10%",                "descuento":"10% Descuento x caja cerrada"},     
     {"codigo":14,   "descripcion":"LOCX",              "mes":"Noviembre", "aumento":"+ 8%",                        "descuento":"10% Descuento x 10 unidades surtidas"}, 
     {"codigo":11,   "descripcion":"WAGNER LOCKHEED",   "mes":"Noviembre", "aumento":"+ 5%",          "descuento":"10% Descuento x 10 unidades surtidas"},
     {"codigo":13,   "descripcion":"TRICO",             "mes":"Octubre", "aumento":"Aumento Desparejo ",         "descuento":"5% Descuento x 10 unidades surtidas"},
-    {"codigo":8,    "descripcion":"RAMA",              "mes":"Dicimebre", "aumento":"+2-8%%",                       "descuento":"Consultar descuento x cantidad"}
+    {"codigo":8,    "descripcion":"RAMA",              "mes":"Diciembre", "aumento":"+2-8%%",                       "descuento":"Consultar descuento x cantidad"}
 ];
 
 module.exports={
