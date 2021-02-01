eee686e5e52bb990f95a0fdd26e5a9227cee4ad8
jimancio <ihourquescos@gmail.com>
2021-02-01 15:49:44 -0300

    aumento motul

--- constantes.js
+++ constantes.js
@@ -1,10 +1,10 @@
 const listas=[
     {"codigo":1,    "descripcion":"FRAM",              "mes":"Enero",   "aumento":"+ 7% nacional / hasta 50% importados",                "descuento":"Consultar descuento x cantidad"},
-    {"codigo":12,   "descripcion":"MOTUL",             "mes":"Enero", "aumento":"+ 5,5% parejo",                "descuento":"10% Descuento x caja cerrada"},       
-    {"codigo":3,    "descripcion":"VALVOLINE",         "mes":"eNERO", "aumento":"+ 7% (salvo aceite de caja desparejo) ",            "descuento":"10% Descuento x caja cerrada"},      
+    {"codigo":12,   "descripcion":"MOTUL",             "mes":"Febrero", "aumento":"+ 4% parejo",                "descuento":"10% Descuento x caja cerrada"},       
+    {"codigo":3,    "descripcion":"VALVOLINE",         "mes":"Enero", "aumento":"+ 7% (salvo aceite de caja desparejo) ",            "descuento":"10% Descuento x caja cerrada"},      
     {"codigo":316,  "descripcion":"PETRONAS",          "mes":"Enero", "aumento":"+ 6% parejo",                        "descuento":"10% Descuento x caja cerrada"},         
     {"codigo":5,    "descripcion":"TOTAL",             "mes":"Noviembre", "aumento":"+ 10%",                "descuento":"10% Descuento x caja cerrada"},     
-    {"codigo":14,   "descripcion":"LOCX",              "mes":"Noviembre", "aumento":"+ 8%",                        "descuento":"10% Descuento x 10 unidades surtidas"}, 
+    {"codigo":14,   "descripcion":"LOCX",              "mes":"Febrero", "aumento":"+ 7%",                        "descuento":"10% Descuento x 10 unidades surtidas"}, 
     {"codigo":11,   "descripcion":"WAGNER LOCKHEED",   "mes":"Noviembre", "aumento":"+ 5%",          "descuento":"10% Descuento x 10 unidades surtidas"},
     {"codigo":13,   "descripcion":"TRICO",             "mes":"Octubre", "aumento":"Aumento Desparejo ",         "descuento":"5% Descuento x 10 unidades surtidas"},
     {"codigo":8,    "descripcion":"RAMA",              "mes":"Diciembre", "aumento":"+2-8%%",                       "descuento":"Consultar descuento x cantidad"}
