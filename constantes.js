4ca843b00ccd62b41343bd4fe4feaf6f37fd161c
jimancio <ihourquescos@gmail.com>
2021-03-01 10:07:29 -0300

    auemtno marzo

--- constantes.js
+++ constantes.js
@@ -1,6 +1,6 @@
 const listas=[
-    {"codigo":1,    "descripcion":"FRAM",              "mes":"Enero",   "aumento":"+ 7% nacional / hasta 50% importados",                "descuento":"Consultar descuento x cantidad"},
-    {"codigo":12,   "descripcion":"MOTUL",             "mes":"Febrero", "aumento":"+ 4% parejo",                "descuento":"10% Descuento x caja cerrada"},       
+    {"codigo":1,    "descripcion":"FRAM",              "mes":"Marzo",   "aumento":"+ 10%",                "descuento":"Consultar descuento x cantidad"},
+    {"codigo":12,   "descripcion":"MOTUL",             "mes":"Marzo", "aumento":"+ 3%",                "descuento":"10% Descuento x caja cerrada"},       
     {"codigo":3,    "descripcion":"VALVOLINE",         "mes":"Febrero", "aumento":" aumento desparejo ",            "descuento":"10% Descuento x caja cerrada"},      
     {"codigo":316,  "descripcion":"PETRONAS",          "mes":"Febrero", "aumento":"+ 5,5% parejo",                        "descuento":"10% Descuento x caja cerrada"},         
     {"codigo":5,    "descripcion":"TOTAL",             "mes":"Noviembre", "aumento":"+ 10%",                "descuento":"10% Descuento x caja cerrada"},     
