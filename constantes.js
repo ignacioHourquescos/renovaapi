cad320e792262be1972827307bec880ffe570600
jimancio <ihourquescos@gmail.com>
2021-03-12 14:32:29 -0300

    saque descuentos

--- constantes.js
+++ constantes.js
@@ -1,13 +1,13 @@
 const listas=[
-    {"codigo":1,    "descripcion":"FRAM",              "mes":"Marzo",   "aumento":"+ 10%",                "descuento":"Consultar descuento x cantidad"},
-    {"codigo":12,   "descripcion":"MOTUL",             "mes":"Marzo", "aumento":"+ 3%",                "descuento":"10% Descuento x caja cerrada"},       
-    {"codigo":3,    "descripcion":"VALVOLINE",         "mes":"Febrero", "aumento":" aumento desparejo ",            "descuento":"10% Descuento x caja cerrada"},      
-    {"codigo":316,  "descripcion":"PETRONAS",          "mes":"Febrero", "aumento":"+ 5,5% parejo",                        "descuento":"10% Descuento x caja cerrada"},         
-    {"codigo":5,    "descripcion":"TOTAL",             "mes":"Noviembre", "aumento":"+ 10%",                "descuento":"10% Descuento x caja cerrada"},     
-    {"codigo":14,   "descripcion":"LOCX",              "mes":"Febrero", "aumento":"+ 7%",                        "descuento":"10% Descuento x 10 unidades surtidas"}, 
-    {"codigo":11,   "descripcion":"WAGNER LOCKHEED",   "mes":"Noviembre", "aumento":"+ 5%",          "descuento":"10% Descuento x 10 unidades surtidas"},
-    {"codigo":13,   "descripcion":"TRICO",             "mes":"Octubre", "aumento":"Aumento Desparejo ",         "descuento":"5% Descuento x 10 unidades surtidas"},
-    {"codigo":8,    "descripcion":"RAMA",              "mes":"Diciembre", "aumento":"+2-8%%",                       "descuento":"Consultar descuento x cantidad"}
+    {"codigo":1,    "descripcion":"FRAM",              "mes":"Marzo",   "aumento":"+ 10%",                  "descuento":"Consultar descuento x cantidad"},
+    {"codigo":12,   "descripcion":"MOTUL",             "mes":"Marzo", "aumento":"+ 3%",                     "descuento":""},       
+    {"codigo":3,    "descripcion":"VALVOLINE",         "mes":"Febrero", "aumento":" aumento desparejo ",    "descuento":""},      
+    {"codigo":316,  "descripcion":"PETRONAS",          "mes":"Febrero", "aumento":"+ 5,5% parejo",          "descuento":""},         
+    {"codigo":5,    "descripcion":"TOTAL",             "mes":"Noviembre", "aumento":"+ 10%",                "descuento":""},     
+    {"codigo":14,   "descripcion":"LOCX",              "mes":"Febrero", "aumento":"+ 7%",                   "descuento":""}, 
+    {"codigo":11,   "descripcion":"WAGNER LOCKHEED",   "mes":"Noviembre", "aumento":"+ 5%",                 "descuento":""},
+    {"codigo":13,   "descripcion":"TRICO",             "mes":"Octubre", "aumento":"Aumento Desparejo ",     "descuento":""},
+    {"codigo":8,    "descripcion":"RAMA",              "mes":"Diciembre", "aumento":"+2-8%%",               "descuento":""}
 ];
 
 module.exports={
