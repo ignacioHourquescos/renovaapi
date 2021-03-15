98da0f8041132d1bbac4292f93d40a6dba969bdb
jimancio <ihourquescos@gmail.com>
2021-03-15 10:06:44 -0300

    aumento smarzo

--- constantes.js
+++ constantes.js
@@ -1,13 +1,13 @@
 const listas=[
     {"codigo":1,    "descripcion":"FRAM",              "mes":"Marzo",   "aumento":"+ 10%",                  "descuento":"Consultar descuento x cantidad"},
     {"codigo":12,   "descripcion":"MOTUL",             "mes":"Marzo", "aumento":"+ 3%",                     "descuento":""},       
-    {"codigo":3,    "descripcion":"VALVOLINE",         "mes":"Febrero", "aumento":" aumento desparejo ",    "descuento":""},      
-    {"codigo":316,  "descripcion":"PETRONAS",          "mes":"Febrero", "aumento":"+ 5,5% parejo",          "descuento":""},         
-    {"codigo":5,    "descripcion":"TOTAL",             "mes":"Noviembre", "aumento":"+ 10%",                "descuento":""},     
+    {"codigo":3,    "descripcion":"VALVOLINE",         "mes":"Marzo", "aumento":" aumento desparejo ",    "descuento":""},      
+    {"codigo":316,  "descripcion":"PETRONAS",          "mes":"Marzo", "aumento":"+ 7% / refrigerantes 29%",          "descuento":""},         
+    {"codigo":5,    "descripcion":"TOTAL",             "mes":"Marzo", "aumento":"+ 6%",                "descuento":""},     
     {"codigo":14,   "descripcion":"LOCX",              "mes":"Febrero", "aumento":"+ 7%",                   "descuento":""}, 
     {"codigo":11,   "descripcion":"WAGNER LOCKHEED",   "mes":"Noviembre", "aumento":"+ 5%",                 "descuento":""},
     {"codigo":13,   "descripcion":"TRICO",             "mes":"Octubre", "aumento":"Aumento Desparejo ",     "descuento":""},
-    {"codigo":8,    "descripcion":"RAMA",              "mes":"Diciembre", "aumento":"+2-8%%",               "descuento":""}
+    {"codigo":8,    "descripcion":"RAMA",              "mes":"Marzo", "aumento":"+2-8%%",               "descuento":""}
 ];
 
 module.exports={
