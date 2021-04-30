533db632b7078b98195994758cf6e64a8f7c325c
jimancio <ihourquescos@gmail.com>
2021-04-30 13:45:11 -0300

    asd

--- constantes.js
+++ constantes.js
@@ -1,13 +1,13 @@
 const listas=[
-    {"codigo":1,    "descripcion":"FRAM",              "mes":"Mayo",   "aumento":"+ 12%",                  "descuento":"Consultar descuento x cantidad"},
-    {"codigo":12,   "descripcion":"MOTUL",             "mes":"Abril", "aumento":"+ 3%",                     "descuento":""},       
-    {"codigo":3,    "descripcion":"VALVOLINE",         "mes":"Mayo", "aumento":"desparejo 5-25% ",    "descuento":""},      
-    {"codigo":316,  "descripcion":"PETRONAS",          "mes":"Mayo", "aumento":"+ 10-20%",          "descuento":""},         
-    {"codigo":5,    "descripcion":"TOTAL",             "mes":"Abril", "aumento":"+ 10%",                "descuento":""},     
-    {"codigo":14,   "descripcion":"LOCX",              "mes":"Febrero", "aumento":"+ 7%",                   "descuento":""}, 
-    {"codigo":11,   "descripcion":"WAGNER LOCKHEED",   "mes":"Noviembre", "aumento":"+ 5%",                 "descuento":""},
-    {"codigo":13,   "descripcion":"TRICO",             "mes":"Octubre", "aumento":"Aumento Desparejo ",     "descuento":""},
-    {"codigo":8,    "descripcion":"RAMA",              "mes":"Marzo", "aumento":"+2-8%%",               "descuento":""}
+    {"codigo":1,    "descripcion":"FRAM",           "mes":"Mayo",   "aumento":"+ 12%",                  "descuento":"Consultar descuento x cantidad"},
+    {"codigo":12,   "descripcion":"MOTUL",          "mes":"Abril", "aumento":"+ 3%",                     "descuento":""},       
+    {"codigo":3,    "descripcion":"VALVOLINE",      "mes":"Mayo", "aumento":"desparejo 5-25% ",    "descuento":""},      
+    {"codigo":316,  "descripcion":"PETRONAS",       "mes":"Mayo", "aumento":"+ 10-20%",          "descuento":""},         
+    {"codigo":5,    "descripcion":"TOTAL",          "mes":"Abril", "aumento":"+ 10%",                "descuento":""},     
+    {"codigo":14,   "descripcion":"LOCX",           "mes":"Febrero", "aumento":"+ 7%",                   "descuento":""}, 
+    {"codigo":11,   "descripcion":"WAGNER LOCKHEED","mes":"Noviembre", "aumento":"+ 5%",                 "descuento":""},
+    {"codigo":13,   "descripcion":"TRICO",          "mes":"Octubre", "aumento":"Aumento Desparejo ",     "descuento":""},
+    {"codigo":8,    "descripcion":"RAMA",           "mes":"Marzo", "aumento":"+2-8%%",               "descuento":""}
 ];
 
 module.exports={
