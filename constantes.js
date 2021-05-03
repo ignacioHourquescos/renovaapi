d9365a723de0525330881b241f242065a93391f8
jimancio <ihourquescos@gmail.com>
2021-05-03 13:06:26 -0300

    asd

--- constantes.js
+++ constantes.js
@@ -5,7 +5,7 @@ const listas=[
     {"codigo":316,  "descripcion":"PETRONAS",       "mes":"Mayo", "aumento":"+ 10-20% +9.5%",          "descuento":""},         
     {"codigo":5,    "descripcion":"TOTAL",          "mes":"Abril", "aumento":"+ 10%",                "descuento":""},     
     {"codigo":14,   "descripcion":"LOCX",           "mes":"Mayo", "aumento":"+ 4%",                   "descuento":""}, 
-    {"codigo":11,   "descripcion":"WAGNER LOCKHEED","mes":"Noviembre", "aumento":"+ 5%",                 "descuento":""},
+    {"codigo":11,   "descripcion":"WAGNER LOCKHEED","mes":"Mayo", "aumento":"+ 10%",                 "descuento":""},
     {"codigo":13,   "descripcion":"TRICO",          "mes":"Octubre", "aumento":"Aumento Desparejo ",     "descuento":""},
     {"codigo":8,    "descripcion":"RAMA",           "mes":"Mayo", "aumento":"+5%",               "descuento":""}
 ];
