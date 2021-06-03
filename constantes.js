2a2b5bc61a235b239c051cede7174ff97a2fb118
jimancio <ihourquescos@gmail.com>
2021-06-03 10:40:53 -0300

    auemntos julio

--- constantes.js
+++ constantes.js
@@ -1,13 +1,13 @@
 const listas=[
-    {"codigo":1,    "descripcion":"FRAM",           "mes":"Ultima Actualización: Mayo",   "aumento":"Aumento ya aplicado: +12%",                  "descuento":"Consultar descuento x cantidad"},
-    {"codigo":12,   "descripcion":"MOTUL",          "mes":"Ultima Actualización: Junio",  "aumento":"Aumento ya aplicado: +7%",                     "descuento":""},       
-    {"codigo":3,    "descripcion":"VALVOLINE",      "mes":"Ultima Actualización: Mayo",   "aumento":"Aumento ya aplicado: desparejo 5-25% ",    "descuento":""},      
-    {"codigo":316,  "descripcion":"PETRONAS",       "mes":"Ultima Actualización: Mayo",   "aumento":"Aumento ya aplicado: + 10-20% +9.5%",          "descuento":""},         
-    {"codigo":5,    "descripcion":"TOTAL",          "mes":"Ultima Actualización: Abril",  "aumento":"Aumento ya aplicado: + 10%",                "descuento":""},     
-    {"codigo":14,   "descripcion":"LOCX",           "mes":"Ultima Actualización: Junio",  "aumento":"Aumento ya aplicado: + 5%",                   "descuento":""}, 
-    {"codigo":11,   "descripcion":"WAGNER LOCKHEED","mes":"Ultima Actualización: Mayo",   "aumento":"Aumento ya aplicado: + 10%",                 "descuento":""},
-    {"codigo":13,   "descripcion":"TRICO",          "mes":"Ultima Actualización: Octubre","aumento":"Aumento ya aplicado: Aumento Desparejo ",     "descuento":""},
-    {"codigo":8,    "descripcion":"RAMA",           "mes":"Ultima Actualización: Mayo",   "aumento":"Aumento ya aplicado: +5%",               "descuento":""}
+    {"codigo":1,    "descripcion":"FRAM",           "mes":"Ultima Actualización:",   "aumento":"Mayo (Aumentó: +12%)",                  "descuento":"Consultar descuento x cantidad"},
+    {"codigo":12,   "descripcion":"MOTUL",          "mes":"Ultima Actualización:",   "aumento":"Junio (Aumentó: +7%)",                     "descuento":""},       
+    {"codigo":3,    "descripcion":"VALVOLINE",      "mes":"Ultima Actualización:",   "aumento":"Mayo (Aumentó: Desparejo 5-25%) ",    "descuento":""},      
+    {"codigo":316,  "descripcion":"PETRONAS",       "mes":"Ultima Actualización:",   "aumento":"Mayo (Aumentó: Desparejo 10-20%)",          "descuento":""},         
+    {"codigo":5,    "descripcion":"TOTAL",          "mes":"Ultima Actualización:",   "aumento":"Abril (Aumentó: + 10%)",                "descuento":""},     
+    {"codigo":14,   "descripcion":"LOCX",           "mes":"Ultima Actualización:",   "aumento":"Junio (Aumentó: + 5%)",                   "descuento":""}, 
+    {"codigo":11,   "descripcion":"WAGNER LOCKHEED","mes":"Ultima Actualización:",   "aumento":"Mayo (Aumentó: + 10%)",                 "descuento":""},
+    {"codigo":13,   "descripcion":"TRICO",          "mes":"Ultima Actualización:",   "aumento":"Marzo (Aumentó: Desparejo) ",     "descuento":""},
+    {"codigo":8,    "descripcion":"RAMA",           "mes":"Ultima Actualización:",   "aumento":"Mayo (Aumentó: +5%)",               "descuento":""}
 ];
 
 module.exports={
