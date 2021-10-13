c3a5bbb75391f1d209b35c0c2e2402177c83d8eb
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2021-10-13 11:20:25 -0300

    auemtnos octubre

--- constantes.js
+++ constantes.js
@@ -1,11 +1,11 @@
 const listas=[
     {"codigo":1,    "descripcion":"FRAM",           "mes2":"Agosto", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 12%)",                  "descuento":"Consultar descuento x cantidad"},
-    {"codigo":12,   "descripcion":"MOTUL",          "mes2":"Agosto", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 6%)",                     "descuento":""},       
-    {"codigo":3,    "descripcion":"VALVOLINE",      "mes2":"Agosto", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: promedio 5%) ",    "descuento":""},      
-    {"codigo":316,  "descripcion":"PETRONAS",       "mes2":"Mayo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: promedio 5%)",          "descuento":""},         
+    {"codigo":12,   "descripcion":"MOTUL",          "mes2":"Octubre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 2%)",                     "descuento":""},       
+    {"codigo":3,    "descripcion":"VALVOLINE",      "mes2":"Octubre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo) ",    "descuento":""},      
+    {"codigo":316,  "descripcion":"PETRONAS",       "mes2":"Septiembre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo)",          "descuento":""},         
     {"codigo":5,    "descripcion":"TOTAL",          "mes2":"Abril", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 10%)",                "descuento":""},     
     {"codigo":14,   "descripcion":"LOCX",           "mes2":"Junio", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 5%)",                   "descuento":""}, 
-    {"codigo":11,   "descripcion":"WAGNER LOCKHEED","mes2":"Septiembre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo) ",                 "descuento":""},
+    {"codigo":11,   "descripcion":"WAGNER LOCKHEED","mes2":"Octubre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo) ",                 "descuento":""},
     {"codigo":13,   "descripcion":"TRICO",          "mes2":"Marzo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo) ",     "descuento":""},
     {"codigo":8,    "descripcion":"RAMA",           "mes2":"Octubre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo)",               "descuento":""}
 ];
