6c70a1a0529d79c87717eb912cceac6ce53b470a
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2021-12-13 12:26:34 -0300

    auemtno dic

--- constantes.js
+++ constantes.js
@@ -1,8 +1,8 @@
 const listas=[
-    {"codigo":1,    "descripcion":"FRAM",           "mes2":"Diciembre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 5%)",                  "descuento":"Consultar descuento x cantidad"},
+    {"codigo":1,    "descripcion":"FRAM",           "mes2":"Diciembre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 5% Desparejo)",                  "descuento":"Consultar descuento x cantidad"},
     {"codigo":12,   "descripcion":"MOTUL",          "mes2":"Diciembre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue del 5%)",                     "descuento":""},       
     {"codigo":3,    "descripcion":"VALVOLINE",      "mes2":"Octubre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo) ",    "descuento":""},      
-    {"codigo":316,  "descripcion":"PETRONAS",       "mes2":"Septiembre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo)",          "descuento":""},         
+    {"codigo":316,  "descripcion":"PETRONAS",       "mes2":"Diciembre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 5% Desparejo)",          "descuento":""},         
     {"codigo":5,    "descripcion":"TOTAL",          "mes2":"Abril", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 10%)",                "descuento":""},     
     {"codigo":14,   "descripcion":"LOCX",           "mes2":"Junio", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 5%)",                   "descuento":""}, 
     {"codigo":11,   "descripcion":"WAGNER LOCKHEED","mes2":"Octubre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo) ",                 "descuento":""},
