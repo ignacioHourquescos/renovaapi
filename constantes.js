801b66f900c035c0450e75e3b84fd5f493d44634
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2022-03-04 16:24:44 -0300

    aumetnos marzo

--- constantes.js
+++ constantes.js
@@ -2,10 +2,10 @@ const listas=[
     {"codigo":1,    "descripcion":"FRAM",           "mes2":"Marzo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 5% ",                  "descuento":"Consultar descuento x cantidad"},
     {"codigo":12,   "descripcion":"MOTUL",          "mes2":"Enero", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue del 6%)",                     "descuento":""},       
     {"codigo":3,    "descripcion":"VALVOLINE",      "mes2":"Octubre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo) ",    "descuento":""},      
-    {"codigo":316,  "descripcion":"PETRONAS",       "mes2":"Diciembre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 5% Desparejo)",          "descuento":""},         
+    {"codigo":316,  "descripcion":"PETRONAS",       "mes2":"Marzo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 9% (salvo synitum 1000 y urania que cambiaron los precios))",          "descuento":""},         
     {"codigo":5,    "descripcion":"TOTAL",          "mes2":"Abril", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 10%)",                "descuento":""},     
     {"codigo":14,   "descripcion":"LOCX",           "mes2":"Junio", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 5%)",                   "descuento":""}, 
-    {"codigo":11,   "descripcion":"WAGNER LOCKHEED","mes2":"Octubre", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo) ",                 "descuento":""},
+    {"codigo":11,   "descripcion":"WAGNER LOCKHEED","mes2":"Marzo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 5%) ",                 "descuento":""},
     {"codigo":13,   "descripcion":"TRICO",          "mes2":"Marzo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo) ",     "descuento":""},
     {"codigo":8,    "descripcion":"RAMA",           "mes2":"Marzo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: Desparejo)",               "descuento":""}
 ];
