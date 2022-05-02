3006d52db96e7f02a065604bd693d37ec329b3c3
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2022-05-02 14:15:31 -0300

    auemntos mayo

--- constantes.js
+++ constantes.js
@@ -1,8 +1,8 @@
 const listas=[
-    {"codigo":1,    "descripcion":"FRAM",           "mes2":"Abril", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue 7% lineal ",                  "descuento":"Consultar descuento x cantidad"},
+    {"codigo":1,    "descripcion":"FRAM",           "mes2":"Mayo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue 6% lineal ",                  "descuento":"Consultar descuento x cantidad"},
     {"codigo":12,   "descripcion":"MOTUL",          "mes2":"Abril", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue 5% lineal)",                     "descuento":""},       
     {"codigo":3,    "descripcion":"VALVOLINE",      "mes2":"Abril", "mes":"Ultima Actualización:",   "aumento":"(desparejo promedio 4%, bidones sinteticos 13% / semi 9%) ",    "descuento":""},      
-    {"codigo":316,  "descripcion":"PETRONAS",       "mes2":"Abril", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 3-11%)",          "descuento":""},         
+    {"codigo":316,  "descripcion":"PETRONAS",       "mes2":"Mayo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 6-25%)",          "descuento":""},         
     // {"codigo":5,    "descripcion":"TOTAL",          "mes2":"Abril", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 10%)",                "descuento":""},     
     {"codigo":27,   "descripcion":"PUMA",           "mes2":"Abril", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: desparejo)",                   "descuento":""}, 
     {"codigo":14,   "descripcion":"LOCX",           "mes2":"Abril", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 5%)",                   "descuento":""}, 
