3c1feb752ebe1896aa504b3ea055ce61ba599dd9
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2022-06-08 15:57:32 -0300

    cambios en aumentos

--- constantes.js
+++ constantes.js
@@ -1,15 +1,64 @@
-const listas=[
-    {"codigo":1,    "descripcion":"FRAM",           "mes2":"Mayo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue 6% lineal ",                  "descuento":"Consultar descuento x cantidad"},
-    {"codigo":12,   "descripcion":"MOTUL",          "mes2":"Junio", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue 4% lineal)",                     "descuento":""},       
-    {"codigo":3,    "descripcion":"VALVOLINE",      "mes2":"Mayo", "mes":"Ultima Actualización:",   "aumento":"(desparejo promedio 4%, bidones sinteticos 13% / semi 9%) ",    "descuento":""},      
-    {"codigo":316,  "descripcion":"PETRONAS",       "mes2":"Mayo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 6-25%)",          "descuento":""},         
-    // {"codigo":5,    "descripcion":"TOTAL",          "mes2":"Abril", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 10%)",                "descuento":""},     
-    {"codigo":27,   "descripcion":"PUMA",           "mes2":"Mayo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: desparejo)",                   "descuento":""}, 
-    {"codigo":14,   "descripcion":"LOCX",           "mes2":"Mayo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 5%)",                   "descuento":""}, 
-    {"codigo":11,   "descripcion":"WAGNER LOCKHEED","mes2":"Marzo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 5%) ",                 "descuento":""},
-    {"codigo":8,    "descripcion":"RAMA",           "mes2":"Mayo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 8-12%)",               "descuento":""}
+const listas = [
+	{
+		codigo: 1,
+		descripcion: "FRAM",
+		mes2: "Mayo",
+		mes: "Ultima Actualización:",
+		aumento: "(El aumento fue 6% lineal ",
+		descuento: "Consultar descuento x cantidad",
+	},
+	{
+		codigo: 12,
+		descripcion: "MOTUL",
+		mes2: "Junio",
+		mes: "Ultima Actualización:",
+		aumento: "(El aumento fue 4% lineal)",
+		descuento: "",
+	},
+	{
+		codigo: 3,
+		descripcion: "VALVOLINE",
+		mes2: "Junio",
+		mes: "Ultima Actualización:",
+		aumento: "(El aumento fue desparejo) ",
+		descuento: "",
+	},
+	{
+		codigo: 316,
+		descripcion: "PETRONAS",
+		mes2: "Mayo",
+		mes: "Ultima Actualización:",
+		aumento: "(El aumento fue: 6-25%)",
+		descuento: "",
+	},
+	// {"codigo":5,    "descripcion":"TOTAL",          "mes2":"Abril", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 10%)",                "descuento":""},
+	{
+		codigo: 27,
+		descripcion: "PUMA",
+		mes2: "Mayo",
+		mes: "Ultima Actualización:",
+		aumento: "(El aumento fue: desparejo)",
+		descuento: "",
+	},
+	{
+		codigo: 14,
+		descripcion: "LOCX",
+		mes2: "Mayo",
+		mes: "Ultima Actualización:",
+		aumento: "(El aumento fue: 5%)",
+		descuento: "",
+	},
+	{
+		codigo: 11,
+		descripcion: "WAGNER LOCKHEED",
+		mes2: "Marzo",
+		mes: "Ultima Actualización:",
+		aumento: "(El aumento fue: 5%) ",
+		descuento: "",
+	},
+	// {"codigo":8,    "descripcion":"RAMA",           "mes2":"Mayo", "mes":"Ultima Actualización:",   "aumento":"(El aumento fue: 8-12%)",               "descuento":""}
 ];
 
-module.exports={
-    listas:listas
-}
+module.exports = {
+	listas: listas,
+};
