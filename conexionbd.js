f16c933d9fa1066524298041c417c1fb0c8ceb33
unknown <ihourquescos@gmail.com>
2023-04-27 12:57:31 -0300

    veriosn

--- conexionbd.js
+++ conexionbd.js
@@ -2,17 +2,17 @@ var sql = require("mssql");
 
 // config for your database
 var config = {
-	user: "rnv_admin",
-	password: "Rnv_12345",
-	server: "200.80.10.160",
-	path: "/RPSISTEMAS",
-	port: 50322,
-	database: "factu_renova",
-	encrypt: false,
+  user: "rnv_admin",
+  password: "Rnv_12345",
+  server: "200.80.10.160",
+  path: "/RPSISTEMAS",
+  port: 50322,
+  database: "factu_renova",
+  encrypt: false,
 };
 
 var connection = sql.connect(config, function (err) {
-	if (err) console.log(err);
+  if (err) console.log(err);
 });
 
 module.exports = connection;
